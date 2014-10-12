cafd38a0a33e7cf4321ff4e1b363d62615e9f109
Leonard Lehmann <leonard.lehmann.ll@gmail.com>
2014-10-13 00:24:52 +0200

    Developing second Activity look for Bluetooth Devices

---
+++ src/com/example/bluetoothController/BluetoothSearcherActivity.java
@@ -0,0 +1,18 @@
+package com.example.bluetoothController;
+
+import com.example.bluetoothcontroller.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BluetoothSearcherActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bluetooth_finder);
+	}
+
+	
+}
---
+++ src/com/example/bluetoothController/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.bluetoothController;
+
+import com.example.bluetoothcontroller.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity {
+	
+	public Button sButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	/** Called when the user clicks the Bluetooth Search Button */
+	public void startBluetoothActivity(View view) {
+		Intent intent = new Intent(this, BluetoothSearcherActivity.class);
+	    startActivity(intent);
+
+	}
+}
