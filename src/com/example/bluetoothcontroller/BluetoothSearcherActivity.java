21c638a83adce2beb0cde4275fe87799e065e666
Leonard Lehmann <leonard.lehmann.ll@gmail.com>
2014-10-12 20:57:44 +0200

    .gitignore repariert

---
+++ src/com/example/bluetoothcontroller/BluetoothSearcherActivity.java
@@ -0,0 +1,5 @@
+package com.example.bluetoothcontroller;
+
+public class BluetoothSearcherActivity {
+
+}
--- src/com/example/bluetoothcontroller/MainActivity.java
+++ src/com/example/bluetoothcontroller/MainActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
+	
+	public Button sButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		sButton = (Button) findViewById(R.id.sButton);
+		sButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
